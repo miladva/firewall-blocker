eaeb51733dc79f9a97afa57845f433da1d1e14e7
milad_vayani <milad_vayani@yahoo.com>
2023-04-15 15:03:33 +0330

    some bugs fixed

--- firewall.sh
+++ firewall.sh
@@ -4,7 +4,6 @@ apt install ufw libapache2-mod-geoip geoip-database
 a2enmod geoip
 apt install geoip-bin
 curl -sSL https://www.ipdeny.com/ipblocks/data/countries/ir.zone | awk '{print "sudo ufw deny out from any to " $1}' | bash
-ufw allow from any to any port 191.96.53.91 proto tcp
 ufw allow from any to any port 22 proto tcp
 ufw enable
 ufw reload
