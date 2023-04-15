41651e49071beb2ed7bac6b22d08bebea18e27e3
milad_vayani <milad_vayani@yahoo.com>
2023-04-15 15:02:22 +0330

    bugs fixed

--- firewall.sh
+++ firewall.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-sudo apt update
-sudo apt install ufw libapache2-mod-geoip geoip-database
-sudo a2enmod geoip
+apt update
+apt install ufw libapache2-mod-geoip geoip-database
+a2enmod geoip
+apt install geoip-bin
 curl -sSL https://www.ipdeny.com/ipblocks/data/countries/ir.zone | awk '{print "sudo ufw deny out from any to " $1}' | bash
-sudo ufw enable
-sudo ufw reload
-
-# Get your IP address
-myip=$(curl -sSL https://ifconfig.me)
-
-# Allow SSH traffic from your IP address
-sudo ufw allow from $myip to any port 2222
+ufw allow from any to any port 191.96.53.91 proto tcp
+ufw allow from any to any port 22 proto tcp
+ufw enable
+ufw reload
